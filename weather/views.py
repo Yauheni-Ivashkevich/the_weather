6780042211aa098cdf7cf0c13fd07cd3a626f57e
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-06-19 12:59:49 +0300

    weather_data

--- weather/views.py
+++ weather/views.py
@@ -4,28 +4,26 @@ from .models import City
 from .forms import CityForm
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=8232076cfb0c3a0a28e2c3f26d07da6d'
+    appid = '8232076cfb0c3a0a28e2c3f26d07da6d'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}units=metric&appid=' + appid
+
+    cities = City.objects.all()
+    weather_data = []
 
     if request.method == "POST":
         form = CityForm(request.POST)
         form.save()
 
-    form = CityForm()
-
-    cities = City.objects.all()
-
-    weather_data = []
 
     for city in cities:
         city_weather = requests.get(url.format(city.name)).json()
-
-        city_weather = {
+        weather = {
             'city': city.name,
-            'temperature': city_weather["main"]["temp"],
+            'temp': city_weather["main"]["temp"],
             'description': city_weather["weather"][0]["description"],
             'icon': city_weather["weather"][0]["icon"],
         }
-        weather_data.append(city_weather)
+        weather_data.append(weather)
 
     context = {'weather_data': weather_data, 'form': CityForm}
     return render(request, 'weather/index.html', context=context)
